5678c85b1f867e12099deaee519b3885f9bd5f00
gambasvb <gambasvb@gmail.com>
2024-09-16 00:32:01 +0000

    feature : add expense data

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -1,3 +1,4 @@
+import 'package:myapp/datetime/date_time_helper.dart';
 import 'package:myapp/models/expense_item.dart';
 
 class ExpenseData {
@@ -20,8 +21,42 @@ class ExpenseData {
   }
 
   // get weekday (mot, tue, etc) from a dateTime object
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return "Mon";
+      case 2:
+        return "Tue";
+      case 3:
+        return "Wed";
+      case 4:
+        return "Thu";
+      case 5:
+        return "Fri";
+      case 6:
+        return "Sat";
+      case 7:
+        return "Sun";
+      default:
+        return "";
+    }
+  }
 
   // get the date for the start of the week (sunday)
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+
+    // get today date
+    DateTime today = DateTime.now();
+
+    // go backward from today to find sunday
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Sun') {
+        startOfWeek = today.subtract(Duration(days: i));
+      }
+    }
+    return startOfWeek!;
+  }
 
   /*
   conver all list of expenses into a daily expnses summary
@@ -40,5 +75,23 @@ class ExpenseData {
     [20230131,$1],
   ]
 
-   */
+  */
+  Map<String, double> calculationDailyExpenseSummary() {
+    Map<String,double> dailyExpenseSummary ={
+
+    };
+
+    for (var expense in overAllExpenseList){
+      String date = convertDateTimeToString(expense.dateTime);
+      double amount = double.parse(expense.amount);
+
+      if(dailyExpenseSummary.containsKey(date)){
+        double currentAmount = dailyExpenseSummary[date]!;
+        dailyExpenseSummary[date] = currentAmount + amount;
+      }else{
+        dailyExpenseSummary.addAll({date:amount});
+      }
+    }
+    return dailyExpenseSummary;
+  }
 }
